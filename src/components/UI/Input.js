8a2b9fccedd6b98b1b7db8fd334b06840f40ca4b
Revanth <revanth54@gmail.com>
2020-04-26 12:12:17 -0600

    added validation to form

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {FormGroup,FormLabel,FormText,FormControl} from 'react-bootstrap';
 import classes from './Input.module.css';
 const input = (props) => {
     let inputEle = null;
-    
+    const inputClasses = ['form-control'];
+    console.log(props)
+    if(!props.validation.valid && props.validation.touched){
+        inputClasses.push(classes.borderSolid);
+        inputClasses.push('border border-danger');
+    }
     switch(props.elementType) {
         case ('input') : 
-            inputEle = <FormControl {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            inputEle = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
     }
 
     return (
             
-                <FormGroup className={classes.formGroup} controlId = { props.id } >
-                    <FormLabel className={classes.formLabel}>{props.label}</FormLabel>
-                        {inputEle}
-                </FormGroup>
+                <div className={'form-group '+classes.formGroup} controlId = { props.id } >
+                    <label for={props.key} className={classes.formLabel}>{props.label}</label>
+                    {inputEle}
+                 </div>
            
     );
 }
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -16,7 +16,8 @@ class Auth extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    valid: false
+                    valid: false,
+                    touched:false
                 }
             },
             lastName: {
@@ -29,7 +30,8 @@ class Auth extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    valid: false
+                    valid: false,
+                    touched:false
                 }
             },
 
@@ -43,7 +45,8 @@ class Auth extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    valid: false
+                    valid: false,
+                    touched:false
                 }
             },
             password: {
@@ -56,7 +59,10 @@ class Auth extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    valid: false
+                    valid: false,
+                    min:8,
+                    max:10,
+                    touched:false
                 }
             },
             reenterPassword: {
@@ -69,19 +75,102 @@ class Auth extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    valid: false
+                    valid: false,
+                    touched:false
                 }
             }
+
+        },
+        errors: {
 
         }
     }
 
-    inputChangedHandler = (event) => {
-        console.log(event);
+    inputChangedHandler = (event,eleName) => {
+        event.preventDefault();
+        this.resetErrors(eleName);
+        const updatedForm = {
+            ...this.state.controls
+        };
+        const value = event.target.value;
+        const updateFormEle = {...updatedForm[eleName]};
+        updateFormEle.value = value;
+        updateFormEle.validation.touched = true;
+        updatedForm.errors = this.validationCheck(eleName,value);
+        if(!updatedForm.errors){
+            updateFormEle.validation.valid = true;
+        }else{
+            updateFormEle.validation.valid = false;
+        }
+        updatedForm[eleName] = updateFormEle;
+        this.setState({controls:updatedForm})
     }
+
+    resetErrors = (name) => {
+        let eleErrors = {...this.state.errors};
+        eleErrors[name] = undefined;
+        this.setState({errors:eleErrors});
+    }
+
+    validationCheck = (name,value) => {
+        const ele = this.state.controls[name];
+        let errors;
+        let requiredMsg = '%name%  is required';
+        let lenMsg = '%name% should be %len% %min% charecter length';
+                if(!this.requiredValidCheck(value,ele.validation.required)){
+                    requiredMsg = requiredMsg.replace('%name%',name);
+                    errors = {
+                        [name] : requiredMsg
+                    }
+                }
+                if(!this.minCheck(value,ele.validation.min)){
+                    lenMsg = lenMsg.replace('%name%',name);
+                    lenMsg = lenMsg.replace('%min%',ele.validation.min);
+                    lenMsg = lenMsg.replace('%len%','minimun');
+                    errors = {
+                        [name] : lenMsg
+                    }
+                }
+                if(!this.maxCheck(value,ele.validation.max)){
+                    lenMsg = lenMsg.replace('%name%',name);
+                    lenMsg = lenMsg.replace('%min%',ele.validation.max);
+                    lenMsg = lenMsg.replace('%len%','maximun');
+                    errors = {
+                        [name] : lenMsg
+                    }
+                }
+               
+        
+        return errors;
+    }
+
+    requiredValidCheck = (value,required) => {
+        let isValid = true;
+        if(required){
+            isValid = value.trim() !== '' && isValid;
+        }
+       return isValid;
+    }
+    minCheck = (value,min) =>{
+        let isValid = true;
+        if(min){
+            isValid = value.length >= min && isValid;
+        }
+        return isValid;
+    } 
+    maxCheck = (value,max) =>{
+        let isValid = true;
+        if(max){
+            isValid = value.length <= max && isValid;
+        }
+        return isValid;
+    } 
     render() {
         const formEleArray = [];
         for (let key in this.state.controls) {
+            if(key==='errors'){
+                continue;
+            }
             formEleArray.push(
                 {
                     id: key,
@@ -94,7 +183,8 @@ class Auth extends Component {
                             <Input key={ele.id} elementType={ele.config.elementType}
                                 label={ele.config.label}
                                 elementConfig={ele.config.elementConfig}
-                                value={ele.config.value}  changed={this.inputChangedHandler}/>
+                                value={ele.config.value}  changed={(event) => this.inputChangedHandler(event,ele.id)}
+                                error={this.state.errors[ele.id]} validation={ele.config.validation}/>
                         )
                         }
                     </Form>);
