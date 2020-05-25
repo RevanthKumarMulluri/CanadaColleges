4d68647b42e7ba835973d2635bb6468212c856f1
Revanth <revanth54@gmail.com>
2020-05-24 20:08:08 -0600

    added ability to store comments to database

--- src/containers/comments/NewComment.js
+++ src/containers/comments/NewComment.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Button from '../../components/UI/button/Button';
+import {forumService} from '../../services/forum.service';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 
 const NewComment = (props) => {
+    const [commentText,setCommentText] = useState('');
+    const { user, loggedIn } = useSelector(state => ({
+        user: state.auth.user,
+        loggedIn: state.auth.loggedIn
+    }));
+    const history = useHistory();
+
+    const addCommentsHandler = () =>{
+       const  comment = {
+           commentText : commentText,
+           email : user.email,
+           displayName : user.displayName
+       }
+        if(commentText){
+            forumService.addComment(props.uniname,comment).then(res => {
+                history.push('/forums/'+props.uniname);
+            }).catch(e => {
+                console.log(e);
+            })
+
+            
+        }
+    }
+
+    const changeHandler = event => {
+        setCommentText(event.target.value);
+    }
+
     return (
         <React.Fragment>
             <Button elementType='btn' data-toggle='modal' data-target='#exampleModal'>
@@ -19,11 +51,11 @@ const NewComment = (props) => {
                         </div>
                         <div className='modal-body mb-3'>
                             <label for='Textarea'>Comment</label>
-                            <textarea className='form-control' id='Textarea' placeholder='Enter Your Comment Here' required></textarea>
+                            <textarea className='form-control' id='Textarea' value = {commentText} onChange={changeHandler} placeholder='Enter Your Comment Here'></textarea>
                         </div>
                         <div className='modal-footer'>
                             <Button elementType='btn' data-dismiss='modal'>Close</Button>
-                            <Button elementType='btn'>Save changes</Button>
+                            <Button elementType='btn' onClick={addCommentsHandler}>Save changes</Button>
                         </div>
                     </div>
                 </div>
