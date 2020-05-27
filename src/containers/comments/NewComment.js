f91002760a140e1b579f6b0074b128d5f4e30c7e
Revanth <revanth54@gmail.com>
2020-05-26 19:30:45 -0600

    add comment handler

--- src/containers/comments/NewComment.js
+++ src/containers/comments/NewComment.js
@@ -1,46 +1,52 @@
-import React,{useState} from 'react';
+import React, { useState, useRef } from 'react';
 import Button from '../../components/UI/button/Button';
-import {forumService} from '../../services/forum.service';
+import { forumService } from '../../services/forum.service';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 
 const NewComment = (props) => {
-    const [commentText,setCommentText] = useState('');
+    const [commentText, setCommentText] = useState('');
     const { user, loggedIn } = useSelector(state => ({
         user: state.auth.user,
         loggedIn: state.auth.loggedIn
     }));
+    const modal = useRef(null);
     const history = useHistory();
+    const [showModal, setShowModal] = useState(true);
+    let modalClose = null;
+    const addCommentsHandler = () => {
+        const comment = {
+            commentText: commentText,
+            email: user.email,
+            displayName: user.displayName
+        }
+        if (commentText) {
+            forumService.addComment(props.uniname, comment).then(res => {
+                  modalClose = {
+                    display: 'none'
+                  };
+                history.push('/forums/' + props.uniname);
 
-    const addCommentsHandler = () =>{
-       const  comment = {
-           commentText : commentText,
-           email : user.email,
-           displayName : user.displayName
-       }
-        if(commentText){
-            forumService.addComment(props.uniname,comment).then(res => {
-                history.push('/forums/'+props.uniname);
             }).catch(e => {
                 console.log(e);
             })
 
-            
+
         }
     }
 
     const changeHandler = event => {
         setCommentText(event.target.value);
     }
-
+    
     return (
         <React.Fragment>
             <Button elementType='btn' data-toggle='modal' data-target='#exampleModal'>
                 Comment
             </Button>
 
-            <div className='modal fade' id='exampleModal' role='dialog' aria-labelledby='ModalLabel' aria-hidden='true'>
+            <div className={'modal'} style={modalClose} ref={modal} id='exampleModal' role='dialog' aria-labelledby='ModalLabel' aria-hidden='true'>
                 <div className='modal-dialog'>
                     <div className='modal-content'>
                         <div className='modal-header'>
@@ -51,7 +57,7 @@ const NewComment = (props) => {
                         </div>
                         <div className='modal-body mb-3'>
                             <label for='Textarea'>Comment</label>
-                            <textarea className='form-control' id='Textarea' value = {commentText} onChange={changeHandler} placeholder='Enter Your Comment Here'></textarea>
+                            <textarea className='form-control' id='Textarea' value={commentText} onChange={changeHandler} placeholder='Enter Your Comment Here'></textarea>
                         </div>
                         <div className='modal-footer'>
                             <Button elementType='btn' data-dismiss='modal'>Close</Button>
