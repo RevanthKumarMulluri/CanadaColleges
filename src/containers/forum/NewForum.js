3619555e3f25bee3a8fcba2d6ae8c8cdd1c6b165
Revanth <revanth54@gmail.com>
2020-07-22 19:44:00 -0600

    took off button import

--- src/containers/forum/NewForum.js
+++ src/containers/forum/NewForum.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '../../components/UI/button/Button';
 import {Link} from 'react-router-dom';
 import classes from './NewForum.module.scss';
 import forum from '../../images/forum.png';
