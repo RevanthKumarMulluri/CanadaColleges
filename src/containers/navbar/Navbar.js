6c4f3754c6c881bdb7cbcfce2dec77ec31496e1a
Revanth <revanth54@gmail.com>
2020-08-22 14:12:13 -0600

    csv to login

--- src/containers/navbar/Navbar.js
+++ src/containers/navbar/Navbar.js
@@ -25,16 +25,27 @@ const Navbar = () => {
 
 
     const UserLoginHandler = () => {
-        let innerHTML =  <Link className='nav-link' to='/login'>SIGN IN</Link>;
+        let innerHTML = <Link className={classes.user_nav_link} to='/login'>
+                            <svg className={classes.user_nav_icon}>
+                                <use xlinkHref={icon + '#icon-login'}> </use>
+                            </svg>
+                            <span className={classes.user_nav_text}>Login</span>
+                        </Link>;
         if(loggedIn){
-            innerHTML =  <div className='nav-item dropdown'>
-                            <a className='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                                 {user.displayName}
-                            </a>
-                            <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
-                                <button className='dropdown-item dropdown btn btn-outline' onClick={logoutHandler}>LOGOUT</button>
-                            </div>
-                        </div>
+            innerHTML =    <React.Fragment>
+                                <div className={classes.user_nav_icon_box}>
+                                    <svg className={classes.user_nav_icon}>
+                                        <use xlinkHref={icon + '#icon-chat'}> </use>
+                                    </svg>
+                                    <span className={classes.user_nav_notifications}>7</span>
+                                </div>
+                                <div className={classes.user_nav_icon_box}>
+                                    <div className={classes.user_nav_user}>
+                                        <img src={pic} alt="userPhoto" className={classes.user_nav_userphoto} />
+                                    </div>
+                                    <span className={classes.user_nav_username}>Revanth Kumar Mulluri</span>
+                                </div>
+                            </React.Fragment>
         }
         return innerHTML;
     }
@@ -44,29 +55,6 @@ const Navbar = () => {
 
     }
     return (
-        // <div className={classes.navcontainer}>
-        //     <div className={classes.bgimage}>
-        //         <nav className={'navbar navbar-expand-lg ' + classes.navColor}>
-        //             <div className={classes.logo}>
-        //                 <img src={logo} className='d-inline-block align-top mx-2' alt='' />
-        //             </div>
-        //             <Link className='navbar-brand' to='/'>
-        //                 CanadaColleges
-        //         </Link>
-        //             <Link className='nav-link' to='/'>STUDY</Link>
-
-        //             <div className={'collapse navbar-collapse col-md-4 ml-auto'} id="navbarNav">
-        //                 <UserLoginHandler />
-        //                 <form className='form-inline my-2 my-lg-0'>
-        //                     <input className='form-control mr-sm-2' type='search' placeholder='Search' aria-label='Search' />
-        //                     <button className='btn btn-outline-light my-2 my-sm-0' type='submit'>Search</button>
-        //                 </form>
-        //             </div>
-        //         </nav>
-        //     </div>
-        // </div>  
-
-
         <header className={classes.nav_header}>
             <img src={logo} className={classes.logo} alt='' />
             <form className={classes.nav_serach}>
@@ -78,22 +66,9 @@ const Navbar = () => {
                 </button>
             </form>
             <nav className={classes.user_nav}>
-                <div className={classes.user_nav_icon_box}>
-                    <svg className={classes.user_nav_icon}>
-                        <use xlinkHref={icon + '#icon-chat'}> </use>
-                    </svg>
-                    <span className={classes.user_nav_notifications}>7</span>
-                </div>
-                <div className={classes.user_nav_icon_box}>
-                    <div className={classes.user_nav_user}>
-                        <img src={pic} alt="userPhoto" className={classes.user_nav_userphoto} />
-                    </div>
-                    <span className={classes.user_nav_username}>Revanth Kumar Mulluri</span>
-                </div>
+                <UserLoginHandler/>
             </nav>
-        </header>
-       
-        
+        </header>  
     );
 }
 
