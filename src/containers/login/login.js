1837d1d844f2ca21eb7d9304efa674e1b70d5142
Revanth <revanth54@gmail.com>
2020-08-22 23:06:39 -0600

    changed css to scss

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import classes from './login.module.css';
+import classes from './login.module.scss';
 import { useDispatch,useSelector } from 'react-redux';
 import { userActions } from '../../actions/creators/user.actions';
 import { Link, useHistory } from 'react-router-dom';
@@ -45,42 +45,60 @@ const Login = () => {
 
 
     return (
-        <div className='col-lg-8 offset-lg-2'>
-            <div className="card card-signin my-5">
-                <div className="card-body">
-                    <h5 className="card-title text-center">Sign In</h5>
-                    <form name='form' onSubmit={submitHandler}>
-                        <div className={'form-group ' + classes.formGroup}>
-                            <label className={classes.formLabel}>Email address</label>
-                            <input type='email' name='username' value={username} onChange={handleChange} className={'form-control' + (submitted && !username ? ' is-invalid' : '')} />
-                            {submitted && !username &&
-                                <div className="invalid-feedback">Username is required</div>
-                            }
-                        </div>
-                        <div className={'form-group ' + classes.formGroup}>
-                            <label className={classes.formLabel}>Password</label>
-                            <input type='password' name='password' value={password} onChange={handleChange} className={'form-control' + (submitted && !password ? ' is-invalid' : '')} />
-                            {submitted && !password &&
-                                <div className='invalid-feedback'>Password is required</div>
-                            }
-                        </div>
-                        <div className={'form-group ' + classes.formGroup}>
-                            <Button disabled={disable} type='submit'>
-                                {/* {loggingIn && <span className="spinner-border spinner-border-sm mr-1"></span>} */}
-                                Sign in
-                            </Button>
-                        </div>
-                    </form>
-                    <div className={'float-middle '+classes.formGroup}>
-                        <Link to={'/register'}>
-                           <Button elementType='btn-outline'>Click here to Register</Button> 
-                        </Link>
-                        <span> OR </span>
-                        <Button elementType='btn-outline'  onClick={googleLoginHandler} type="submit"> <FontAwesomeIcon  icon={['fab', 'google']} /> Sign in with Google</Button>
+        // <div className='col-lg-8 offset-lg-2'>
+        //     <div className="card card-signin my-5">
+        //         <div className="card-body">
+        //             <h5 className="card-title text-center">Sign In</h5>
+        //             <form name='form' onSubmit={submitHandler}>
+        //                 <div className={'form-group ' + classes.formGroup}>
+        //                     <label className={classes.formLabel}>Email address</label>
+        //                     <input type='email' name='username' value={username} onChange={handleChange} className={'form-control' + (submitted && !username ? ' is-invalid' : '')} />
+        //                     {submitted && !username &&
+        //                         <div className="invalid-feedback">Username is required</div>
+        //                     }
+        //                 </div>
+        //                 <div className={'form-group ' + classes.formGroup}>
+        //                     <label className={classes.formLabel}>Password</label>
+        //                     <input type='password' name='password' value={password} onChange={handleChange} className={'form-control' + (submitted && !password ? ' is-invalid' : '')} />
+        //                     {submitted && !password &&
+        //                         <div className='invalid-feedback'>Password is required</div>
+        //                     }
+        //                 </div>
+        //                 <div className={'form-group ' + classes.formGroup}>
+        //                     <Button disabled={disable} type='submit'>
+        //                         {/* {loggingIn && <span className="spinner-border spinner-border-sm mr-1"></span>} */}
+        //                         Sign in
+        //                     </Button>
+        //                 </div>
+        //             </form>
+        //             <div className={'float-middle '+classes.formGroup}>
+        //                 <Link to={'/register'}>
+        //                    <Button elementType='btn-outline'>Click here to Register</Button> 
+        //                 </Link>
+        //                 <span> OR </span>
+        //                 <Button elementType='btn-outline'  onClick={googleLoginHandler} type="submit"> <FontAwesomeIcon  icon={['fab', 'google']} /> Sign in with Google</Button>
                        
-                    </div>
+        //             </div>
               
+        //         </div>
+        //     </div>
+        // </div>
+        <div className={classes.header}>
+            <div className={classes.login}>
+            <form className={classes.login_form}>
+                <h5 className={classes.login_form_title}>Login</h5>
+                <div className={classes.login_form_group}>
+                    <input type='text' className={classes.login_form_group_input} onfocus='this.placeholder=""' placeholder='Email or User Name' />
+                    <label className={classes.login_form_group_label_username}></label>
                 </div>
+                <div className={classes.login_form_group}>
+                    <input type='password' className={classes.login_form_group_input} placeholder='Password' />
+                    <label className={classes.login_form_group_label_password}></label>
+                </div>
+            </form>
+            </div>
+            <div>
+
             </div>
         </div>
 
